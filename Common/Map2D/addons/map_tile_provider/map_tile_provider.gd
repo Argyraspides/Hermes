85d22172ee57c0f22161237cf9aed0ad7c5fd0fb
Argyraspides <joshivinayak441@gmail.com>
2024-12-23 23:12:18 +1300

    Fixed import paths in map tile provider

--- Common/Map2D/addons/map_tile_provider/map_tile_provider.gd
+++ Common/Map2D/addons/map_tile_provider/map_tile_provider.gd
@@ -2,46 +2,46 @@
 extends EditorPlugin
 
 
-const MapTileResourceLoader = preload("res://addons/map_tile_provider/map_tile_resource_loader.gd")
-const MapTileResourceSaver = preload("res://addons/map_tile_provider/map_tile_resource_saver.gd")
+const MapTileResourceLoader = preload("res://Common/Map2D/addons/map_tile_provider/map_tile_resource_loader.gd")
+const MapTileResourceSaver = preload("res://Common/Map2D/addons/map_tile_provider/map_tile_resource_saver.gd")
 
 
 var _loader: MapTileResourceLoader
 var _saver: MapTileResourceSaver
 
 
-func _enter_tree():
+func _enter_tree() -> void:
 	# initialize tile providers
 	add_custom_type(
 			"MapProvider", "Node",
-			preload("res://addons/map_tile_provider/map_provider.gd"),
-			preload("res://addons/map_tile_provider/map.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/map_provider.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/map.svg")
 	)
 	add_custom_type(
 			"BingMapProvider", "Node", 
-			preload("res://addons/map_tile_provider/bing_map_provider.gd"),
-			preload("res://addons/map_tile_provider/map.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/bing_map_provider.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/map.svg")
 	)
 	add_custom_type(
 			"MapboxMapProvider", "Node",
-			preload("res://addons/map_tile_provider/mapbox_map_provider.gd"),
-			preload("res://addons/map_tile_provider/map.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/mapbox_map_provider.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/map.svg")
 	)
 	add_custom_type(
 			"MapQuestMapProvider", "Node",
-			preload("res://addons/map_tile_provider/mapquest_map_provider.gd"),
-			preload("res://addons/map_tile_provider/map.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/mapquest_map_provider.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/map.svg")
 	)
 	# initialize the loader
 	add_custom_type(
 			"MapTile", "Resource",
-			preload("res://addons/map_tile_provider/map_tile.gd"),
-			preload("res://addons/map_tile_provider/tile.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/map_tile.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/tile.svg")
 	)
 	add_custom_type(
 			"MapTileLoader", "Node",
-			preload("res://addons/map_tile_provider/map_tile_loader.gd"),
-			preload("res://addons/map_tile_provider/loader.svg")
+			preload("res://Common/Map2D/addons/map_tile_provider/map_tile_loader.gd"),
+			preload("res://Common/Map2D/addons/map_tile_provider/loader.svg")
 	)
 	_loader = MapTileResourceLoader.new()
 	ResourceLoader.add_resource_format_loader(_loader)
@@ -50,7 +50,7 @@ func _enter_tree():
 	ResourceSaver.add_resource_format_saver(_saver)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	# clean up the saver
 	ResourceSaver.remove_resource_format_saver(_saver)
 	# clean up the loader
