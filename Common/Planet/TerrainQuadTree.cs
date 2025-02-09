d7d56b48ee37d698a68467bf569d337dafde5f2a
Argyraspides <joshivinayak441@gmail.com>
2025-02-09 19:55:41 +1300

    Converted terrainquadtree from 2d array to normal tree representation

--- Common/Planet/TerrainQuadTree.cs
+++ Common/Planet/TerrainQuadTree.cs
@@ -22,6 +22,50 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+/*
+ TODO(Argyraspides, 09/02/2025): Current issues:
+
+ The terrain quad tree works but its implemented pretty poorly IMO. Right now, the quad tree is represented as a
+ 2D array where each row represents a particular level, containing the entire level of quadtree nodes.
+
+ Suppose we zoom in and are looking at a section of a planet which should belong, say, in the middle of a level
+ then we will need to create (2^zoom / 2) quadtree nodes to accomodate for this request which is unreasonable as
+ we aren't even looking at all those other ones anyway and hence they shouldn't exist.
+
+ This means in the worst case, at zoom level 20, we could potentially have up to:
+
+ (2^0 * 2^0) + (2^1 * 2^1) + (2^2 * 2^2) ... + (2^20 * 2^20) nodes in the quadtree, even
+ though the user may have only been looking at a very specific region of the Earth and never
+ looked anywhere else.
+
+ Change the quadtree implementation so that it is a true tree structure that looks like:
+
+   public sealed class TerrainQuadTreeNode
+   {
+       // The TerrainChunk associated with this node.
+       public TerrainChunk Chunk;
+       // List of child terrain chunks.
+       public List<TerrainQuadTreeNode> ChildNodes;
+       public TerrainQuadTreeNode(TerrainChunk chunk)
+       {
+           Chunk = chunk;
+           ChildNodes = new List<TerrainQuadTreeNode>(4);
+       }
+   }
+
+Though the thing is, currently there is a high-resolution texture which covers the entirety of Earth's surface
+at zoom level 6 (4096 tiles) bundled with the application. At startup this is loaded in order to prevent
+network requests to the backend servers
+
+This means that at startup, we will somehow have to BFS our way to the entire leaf node level and call .Load()
+on all the chunks. Since this is initialization this may not be a problem as it only occurs once per application startup, though
+I'm not sure about this.
+
+Although this true-tree representation saves on memory, it means we are going to be constantly traversing the tree and loading/unloading
+chunks as the user pans around, so we will be doing BFS like possibly everytime the user is panning around and zooming in which is not ideal,
+but again I don't know if this will be an issue.
+
+ */
 public class TerrainQuadTree
 {
     /// <summary>
@@ -32,24 +76,18 @@ public class TerrainQuadTree
     {
         // The TerrainChunk associated with this node.
         public TerrainChunk Chunk;
-
-        // List of child terrain chunks.
-        public List<TerrainChunk> ChildChunks;
+        public TerrainQuadTreeNode[] ChildNodes;
 
         public TerrainQuadTreeNode(TerrainChunk chunk)
         {
             Chunk = chunk;
-            // We expect up to 4 children.
-            ChildChunks = new List<TerrainChunk>(4);
+            ChildNodes = new TerrainQuadTreeNode[4];
         }
     }
 
-    /// <summary>
-    /// Each row in this list represents a zoom level, and contains all the quadtree nodes at that level.
-    /// </summary>
-    private List<List<TerrainQuadTreeNode>> m_terrainQuadTreeNodes { get; set; }
+    private TerrainQuadTreeNode m_rootNode;
 
-    private List<TerrainQuadTreeNode> m_nullIslandNodes {get; set; }
+    private List<TerrainQuadTreeNode> m_nullIslandNodes { get; set; }
 
     /// <summary>
     /// Splits the quadtree node associated with the particular latitude/longitude.
@@ -59,8 +97,6 @@ public class TerrainQuadTree
     /// <param name="zoom">Zoom level to split at.</param>
     public void SplitQuadTree(float latitude, float longitude, int zoom)
     {
-        // TODO: Create
-
     }
 
     /// <summary>
@@ -78,34 +114,21 @@ public class TerrainQuadTree
     /// <param name="maxZoom">The maximum zoom level (depth) of the quadtree.</param>
     public void InitializeQuadTree(int maxZoom)
     {
-
-        // Initialize the outer list.
-        m_terrainQuadTreeNodes = new List<List<TerrainQuadTreeNode>>();
         m_nullIslandNodes = new List<TerrainQuadTreeNode>();
-
-        TerrainQuadTreeNode rootNode = new TerrainQuadTreeNode(new TerrainChunk(
+        m_rootNode = new TerrainQuadTreeNode(new TerrainChunk(
             new MapTile(0, 0, 0)
         ));
 
         var queue = new Queue<(TerrainQuadTreeNode, int parentRow, int parentCol)>();
-        queue.Enqueue((rootNode, 0, 0));
+        queue.Enqueue((m_rootNode, 0, 0));
 
-        for (int currZoomLevel = 0; currZoomLevel <= maxZoom; currZoomLevel++)
+        for (int currZoomLevel = 1; currZoomLevel < maxZoom; currZoomLevel++)
         {
             // For all nodes in the queue, generate all their child nodes
             int queueSize = (int)Math.Pow(4, currZoomLevel);
-            m_terrainQuadTreeNodes.Add(new List<TerrainQuadTreeNode>());
-
             for (int i = 0; i < queueSize; i++)
             {
-                // Parent node
-                (TerrainQuadTreeNode node, int parentRow, int parentCol) = queue.Dequeue();
-                m_terrainQuadTreeNodes[currZoomLevel].Add(node);
-
-                if (currZoomLevel == maxZoom)
-                {
-                    continue;
-                }
+                (TerrainQuadTreeNode parentNode, int parentRow, int parentCol) = queue.Dequeue();
 
                 // Generate children for this node
                 for (int c = 0; c < 4; c++ /* haha C++ in C# */)
@@ -129,7 +152,6 @@ public class TerrainQuadTree
                     }
 
 
-
                     float childLat = (float)MapUtils.MapTileToLatitude(childRow, currZoomLevel + 1);
                     float childLatRange = (float)MapUtils.TileToLatRange(childRow, currZoomLevel + 1);
                     float childLatHalfRange = childLatRange / 2;
@@ -140,17 +162,16 @@ public class TerrainQuadTree
                     float childLonHalfRange = childLonRange / 2;
                     childLon += childLonHalfRange;
 
-                    TerrainChunk childChunk = new TerrainChunk(
-                            new MapTile(childLat, childLon, currZoomLevel + 1)
-                        );
-
+                    MapTile childMapTile = new MapTile(childLat, childLon, currZoomLevel + 1);
+                    TerrainChunk childChunk = new TerrainChunk(childMapTile);
                     TerrainQuadTreeNode childNode = new TerrainQuadTreeNode(childChunk);
-                    node.ChildChunks.Add(childChunk);
+                    parentNode.ChildNodes[c] = childNode;
+
                     queue.Enqueue((childNode, childRow, childCol));
 
-                    if(childRow == (1 << (currZoomLevel + 1)) / 2)
+                    if (childRow == (1 << (currZoomLevel + 1)) / 2)
                     {
-                        if(childCol == (1 << (currZoomLevel + 1)) / 2)
+                        if (childCol == (1 << (currZoomLevel + 1)) / 2)
                         {
                             m_nullIslandNodes.Add(childNode);
                         }
@@ -162,26 +183,21 @@ public class TerrainQuadTree
 
     public List<TerrainQuadTreeNode> GetQuadTreeLevel(int zoomLevel)
     {
-        if (zoomLevel >= m_terrainQuadTreeNodes.Count || zoomLevel < 0)
-        {
-            throw new IndexOutOfRangeException("In TerrainQuadTree: Trying to grab quadtree level that doesn't exist");
-        }
-        return m_terrainQuadTreeNodes[zoomLevel];
+        throw new NotImplementedException();
     }
 
     public TerrainQuadTreeNode GetCenter(int zoomLevel)
     {
-        if(zoomLevel >= m_nullIslandNodes.Count)
+        if (zoomLevel >= m_nullIslandNodes.Count)
         {
             throw new IndexOutOfRangeException();
         }
+
         return m_nullIslandNodes[zoomLevel];
     }
 
     public List<TerrainQuadTreeNode> GetLastQuadTreeLevel()
     {
-        return m_terrainQuadTreeNodes[m_terrainQuadTreeNodes.Count - 1];
+        throw new NotImplementedException();
     }
-
 }
-
