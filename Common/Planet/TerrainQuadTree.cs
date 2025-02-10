9c5d7d0352fff8d4fde60472960f702f0ca7023e
Argyraspides <joshivinayak441@gmail.com>
2025-02-10 22:36:13 +1300

    Drafted LoD plan

--- Common/Planet/TerrainQuadTree.cs
+++ Common/Planet/TerrainQuadTree.cs
@@ -23,275 +23,124 @@ using System.Collections.Generic;
 using Godot;
 
 /*
- TODO(Argyraspides, 09/02/2025): Current issues:
+ TODO(Argyraspides, 10/02/2025):
 
-Currently there is a high-resolution texture which covers the entirety of Earth's surface
-at zoom level 6 (4096 tiles) bundled with the application. At startup this is loaded in order to prevent
-network requests to the backend servers
+Here's how I think this should work:
 
-This means that at startup, we will somehow have to BFS our way to the entire leaf node level and call .Load()
-on all the chunks. Since this is initialization this may not be a problem as it only occurs once per application startup, though
-I'm not sure about this.
+Note that when I say "split", I just mean loading up children and making the parent invisible. Remember we want to make things as smooth as
+possible for the user, so as they zoom out, we arent just discarding the entire tree below the zoom level that represents the map tiles
+the camera can see. Conversely, we aren't discarding the entire tree above us as we zoom in, either (in fact this would make it completely
+un-traversable anyways as now we have no root to start BFS from). This makes it so that if the user is zooming in/out a lot, we can quickly
+load/unload chunks as they are still in the tree, we just need to enable/disable visiblity or something equivalent. Thus, we are simply
+enabling/disabling parents and children as we go along. If we ever exceed our maximum allowed nodes, then we can start considering culling parts of the tree. We must always cull downwards, as
+culling upwards will break the tree (potentially removing the root node) and we cannot traverse it anymore.
 
-Although this true-tree representation saves on memory, it means we are going to be constantly traversing the tree and loading/unloading
-chunks as the user pans around, so we will be doing BFS like possibly everytime the user is panning around and zooming in which is not ideal,
-but again I don't know if this will be an issue.
+- Every "game" loop, we will traverse the entire quadtree via BFS. For every node we encounter, we will check:
+
+    - Is the node currently loaded in the scene tree? If so, we can check if it needs to be split/merged. If it doesn't need to be split,
+    we can just leave it. If it doesn't need to be split, AND we have exceeded the maximum amount of nodes, then we can cut off the entire
+    sub-tree below this node as they currently aren't visible anyways, and we need to clean up.
+        - If we do need to split, then we can just split, and disable the parent.
+
+    - Is the node a leaf node and NOT loaded in the scene tree? If so, check the maximum amount of allowed nodes. If
+    we have exceeded it, then remove the node entirely as it is not needed.
+
+This is all I can think of in terms of removing unnecessary nodes. This will also make tree traversal more efficient as we have a cap on
+the maximum number of nodes.
+
+Since this is going to be within some Planet class (planets will have a quadtree that they will use to manage their
+surface LoD system), then TerrainQuadTree must be part of the main Godot scene tree and thus must be a part of the main thread.
+The constant BFS across every loop warrants this particular function loop to be offloaded to another thread. Therefore,
+the UpdateQuadTree() should run in a separate thread.
+
+The way this will be used is:
+
+- A planet class will have this quadtree class instance
+- A planet will give the quadtree the game camera
+- The terrain quad tree constantly updates based on the camera
+- The planet class is happy as all LoD and terrain management is done by the terrainquadtree,
+meanwhile the planet can focus on other stuff like cool atmospheric effects, orbits, etc.
 
  */
 public partial class TerrainQuadTree : Node
 {
-    /// <summary>
-    /// Represents a node in a terrain quadtree structure.
-    /// Each node can have exactly 0 or 4 child chunks.
-    /// </summary>
-    public sealed class TerrainQuadTreeNode
+    private Camera3D m_camera;
+    private TerrainQuadTreeNode m_rootNode;
+
+    // Maximum amount of nodes allowed in our quadtree until
+    // unused nodes are culled
+    private long m_maxNodes;
+
+    TerrainQuadTree(Camera3D camera, int maxNodes = (1 << 10))
     {
-        // The TerrainChunk associated with this node.
-        public TerrainChunk Chunk;
-        public TerrainQuadTreeNode[] ChildNodes;
+        m_camera = camera;
+    }
+
+    // Constantly performs BFS on the quadtree and splits/merge terrain quad tree nodes
+    // based on the camera
+    public void UpdateQuadTree(Camera3D camera3D)
+    {
+    }
+
+    // Initializes the quadtree to a particular zoom level.
+    public void InitializeQuadTree(int zoomLevel)
+    {
+    }
+
+    // Determines if the input terrain quad tree node should be split or not based on the camera distance,
+    // what it can see, etc.
+    private bool ShouldSplit(TerrainQuadTreeNode node)
+    {
+        return false;
+    }
+
+    // Splits the terrain quad tree node into four children, and makes its parent invisible
+    private void Split(TerrainQuadTreeNode node)
+    {
+    }
+
+    // The input parameter is the parent quadtree whose children we wish to merge into it. Works by removing the children
+    // from the scene tree/making them invisible, and then toggling itself to be visible or whatever
+    private void Merge(TerrainQuadTreeNode parent)
+    {
+    }
+
+    // Called whenever we exceed the max number of allowed nodes in our quadtree. Completely destroys a quadtree node
+    // by removing it from the scene tree and discarding it entirely. If we ever want to load this particular node again,
+    // we will have to make it from scratch and the map tile data will have to be fetched from the map tile cache which is already
+    // implemented elsewhere
+    private void RemoveQuadTreeNode(TerrainQuadTreeNode node)
+    {
+    }
+
+    // Removes an entire subtree from the quadtree, including the parent
+    private void RemoveSubQuadTree(TerrainQuadTreeNode parent)
+    {
+
+    }
+
+    private sealed class TerrainQuadTreeNode
+    {
+        public TerrainChunk Chunk { get; set; }
+        public TerrainQuadTreeNode[] ChildNodes { get; set; }
+        public TerrainQuadTreeNode ParentNode { get; set; }
+
+        // Tells us if the quadtree node is loaded in the actual scene.
+        // When we are traversing the quadtree, if we ever encounter something that *is* loaded
+        // in the scene, then we know that this particular node is visible to the camera. It is at
+        // this point we can call the functions ShouldSplit(), and Split() if we need to split,
+        // and ShouldMerge() and Merge() if we need to merge
+        // So this flag helps us optimize performance by not calling those functions on nodes
+        // that aren't currently visible and hence don't need to be considered for splitting/merging.
+        // Once we do encounter isLoadedInScene to be true, then we can also not add the children of this
+        // node to our BFS, so we save on space as well
+        public bool isLoadedInScene { get; set; }
 
         public TerrainQuadTreeNode(TerrainChunk chunk)
         {
             Chunk = chunk;
             ChildNodes = new TerrainQuadTreeNode[4];
+            isLoadedInScene = false;
         }
-    }
-
-    private TerrainQuadTreeNode m_rootNode;
-
-    private int m_currentDepth;
-
-    private List<TerrainQuadTreeNode> m_nullIslandNodes { get; set; }
-
-    /// <summary>
-    /// Splits the quadtree node by determining lat/lon tile coordinates of its four children,
-    /// then injecting the children into the parent node's ChildNodes array
-    /// </summary>
-    /// <param name="parentNode"> The quadtree node to be split </param>
-    public void SplitQuadTreeNode(TerrainQuadTreeNode parentNode)
-    {
-        int parentRow = parentNode.Chunk.MapTile.LatitudeTileCoo;
-        int parentCol = parentNode.Chunk.MapTile.LongitudeTileCoo;
-        int currZoomLevel = parentNode.Chunk.MapTile.ZoomLevel;
-        // Generate children for this node. 0 = top left, 1 = top right, 2 = bottom left, 3 = bottom right
-        for (int c = 0; c < 4; c++ /* haha C++ in C# */)
-        {
-            // Top left
-            int childRow = parentRow * 2;
-            int childCol = parentCol * 2;
-
-            // Top right or bottom right respectively
-            childCol += (c == 1 || c == 3) ? 1 : 0;
-            // Bottom left or bottom right respectively
-            childRow += (c == 2 || c == 3) ? 1 : 0;
-
-            // The lat of a map tile is at its center, while MapUtils gives back
-            // latitude of the northern edge, so we compute half the range and subtract to get the
-            // center lat
-            float childLat = (float)MapUtils.MapTileToLatitude(childRow, currZoomLevel + 1);
-            float childLatRange = (float)MapUtils.TileToLatRange(childRow, currZoomLevel + 1);
-            float childLatHalfRange = childLatRange / 2;
-            childLat -= childLatHalfRange;
-
-            // The lat of a map tile is at its center, while the MapUtils gives back
-            // longitude of the western edge, so we compute half the range and subtract to get the
-            // center lon
-            float childLon = (float)MapUtils.MapTileToLongitude(childCol, currZoomLevel + 1);
-            float childLonRange = (float)MapUtils.TileToLonRange(currZoomLevel + 1);
-            float childLonHalfRange = childLonRange / 2;
-            childLon += childLonHalfRange;
-
-            // Create child node and give it to our parent
-            MapTile childMapTile = new MapTile(childLat, childLon, currZoomLevel + 1);
-            TerrainChunk childChunk = new TerrainChunk(childMapTile);
-            TerrainQuadTreeNode childNode = new TerrainQuadTreeNode(childChunk);
-            parentNode.ChildNodes[c] = childNode;
-        }
-    }
-
-    /// <summary>
-    /// Finds a quadtree node based on latitude, longitude, and zoom level with DFS and splits it
-    /// into four pieces
-    /// </summary>
-    /// <param name="latitude">Latitude of the quadtree to split in radians</param>
-    /// <param name="longitude">Longitude of the quadtree to split in radians</param>
-    /// <param name="zoom">Zoom level of the quadtree node to split (also represents the level of the quadtree itself)</param>
-    public void SplitQuadTreeNodeDFS(float latitude, float longitude, int zoom)
-    {
-        TerrainQuadTreeNode node = FindQuadTreeNodeDFS(latitude, longitude, zoom);
-        SplitQuadTreeNode(node);
-    }
-
-    public void SplitAndLoadQuadTreeNode(float latitude, float longitude, int zoom)
-    {
-        TerrainQuadTreeNode parent = FindQuadTreeNodeDFS(latitude, longitude, zoom);
-        SplitQuadTreeNode(parent);
-
-        parent.Chunk.ToggleVisible(false);
-
-        for (int c = 0; c < 4; c++)
-        {
-            ProcessLeafNode(parent.ChildNodes[c]);
-        }
-    }
-
-    /// <summary>
-    /// Finds a quadtree node at a specific latitude, longitude, and zoom level using the DFS algorithm.
-    /// No stack is allocated for DFS.
-    /// </summary>
-    /// <param name="latitude">Latitude in radians</param>
-    /// <param name="longitude">Longitude in radians</param>
-    /// <param name="zoom">Zoom level of the quadtree node to split (also represents the level of the quadtree itself)</param>
-    /// <returns>The located quadtree node</returns>
-    public TerrainQuadTreeNode FindQuadTreeNodeDFS(float latitude, float longitude, int zoom)
-    {
-        if (zoom == 0) { return m_rootNode; }
-
-        TerrainQuadTreeNode currentNode = m_rootNode;
-
-        for (int i = 1; i <= zoom - 1; i++)
-        {
-            int currLatTileCoo = MapUtils.LatitudeToTileCoordinateMercator(latitude, i);
-            int currLonTileCoo = MapUtils.LongitudeToTileCoordinateMercator(longitude, i);
-
-            // Latitude and longitude tile coordinates of where the corresponding tile would be
-            // the next level down
-            int nextLatTileCoo = MapUtils.LatitudeToTileCoordinateMercator(latitude, i + 1);
-            int nextLonTileCoo = MapUtils.LongitudeToTileCoordinateMercator(longitude, i + 1);
-
-            // (2x, 2y) = Top right corner
-            if (nextLatTileCoo == currLatTileCoo * 2 && nextLonTileCoo == currLonTileCoo * 2)
-            {
-                currentNode = m_rootNode.ChildNodes[0];
-            }
-            // (2x + 1, 2y) = Top left corner
-            else if (nextLatTileCoo == currLatTileCoo * 2 + 1 && nextLonTileCoo == currLonTileCoo * 2)
-            {
-                currentNode = m_rootNode.ChildNodes[1];
-            }
-            // (2x, 2y + 1) = Bottom left corner
-            else if (nextLatTileCoo == currLatTileCoo * 2 && nextLonTileCoo == currLonTileCoo * 2 + 1)
-            {
-                currentNode = m_rootNode.ChildNodes[2];
-            }
-            // (2x + 1, 2y + 1) = Bottom right corner
-            else if (nextLatTileCoo == currLatTileCoo * 2 + 1 && nextLonTileCoo == currLonTileCoo * 2 + 1)
-            {
-                currentNode = m_rootNode.ChildNodes[3];
-            }
-        }
-
-        return currentNode;
-    }
-
-    /// <summary>
-    /// Initializes a quadtree of the Earth up to a maximum zoom level using BFS.
-    /// Each level of the quadtree is processed completely before moving on to the next.
-    /// </summary>
-    /// <param name="maxZoom">The maximum zoom level (depth) of the quadtree.</param>
-    public void InitializeQuadTree(int maxZoom)
-    {
-        m_nullIslandNodes = new List<TerrainQuadTreeNode>();
-
-        // Create the root node (zoom level 0).
-        m_rootNode = new TerrainQuadTreeNode(new TerrainChunk(new MapTile(0, 0, 0)));
-
-        // Queue for BFS: each entry holds a node and its grid coordinates.
-        var nodeQueue = new Queue<(TerrainQuadTreeNode node, int row, int col)>();
-        nodeQueue.Enqueue((m_rootNode, 0, 0));
-
-        // Build the quadtree level by level.
-        for (int currZoomLevel = 0; currZoomLevel < maxZoom; currZoomLevel++)
-        {
-            // The children will be at zoom level (currZoomLevel + 1)
-            int childZoom = currZoomLevel + 1;
-            // At the current level there are exactly 4^currZoomLevel nodes.
-            int nodesAtCurrentLevel = (int)Math.Pow(4, currZoomLevel);
-
-            for (int i = 0; i < nodesAtCurrentLevel; i++)
-            {
-                var (parentNode, parentRow, parentCol) = nodeQueue.Dequeue();
-
-                // Create child nodes for each quadrant (0 = top-left, 1 = top-right,
-                // 2 = bottom-left, 3 = bottom-right)
-                foreach (var (rowOffset, colOffset, quadrantIndex) in GetChildQuadrants())
-                {
-                    int childRow = parentRow * 2 + rowOffset;
-                    int childCol = parentCol * 2 + colOffset;
-
-                    // Compute the center coordinates of the child tile.
-                    float childLat = (float)MapUtils.ComputeCenterLatitude(childRow, childZoom);
-                    float childLon = (float)MapUtils.ComputeCenterLongitude(childCol, childZoom);
-
-                    // Create the map tile, terrain chunk and quadtree node.
-                    var childMapTile = new MapTile(childLat, childLon, childZoom);
-                    var childChunk = new TerrainChunk(childMapTile);
-                    var childNode = new TerrainQuadTreeNode(childChunk);
-                    parentNode.ChildNodes[quadrantIndex] = childNode;
-
-                    // Enqueue the child node for further subdivision.
-                    nodeQueue.Enqueue((childNode, childRow, childCol));
-
-                    // Check if this node represents "null island" (i.e. (0,0) center).
-                    int midIndex = (1 << childZoom) / 2;
-                    if (childRow == midIndex && childCol == midIndex)
-                    {
-                        m_nullIslandNodes.Add(childNode);
-                    }
-                }
-            }
-        }
-
-        // Process remaining nodes in the queue (leaf nodes).
-        while (nodeQueue.Count > 0)
-        {
-            var (node, _, _) = nodeQueue.Dequeue();
-            ProcessLeafNode(node);
-        }
-
-        m_currentDepth = maxZoom;
-    }
-
-    /// <summary>
-    /// Returns the row and column offsets for each child quadrant.
-    /// </summary>
-    private IEnumerable<(int rowOffset, int colOffset, int quadrantIndex)> GetChildQuadrants()
-    {
-        // Order: top-left, top-right, bottom-left, bottom-right.
-        yield return (0, 0, 0);
-        yield return (0, 1, 1);
-        yield return (1, 0, 2);
-        yield return (1, 1, 3);
-    }
-
-
-    /// <summary>
-    /// Processes a leaf node by generating its mesh and loading the terrain chunk.
-    /// </summary>
-    private void ProcessLeafNode(TerrainQuadTreeNode node)
-    {
-        var tile = node.Chunk.MapTile;
-        ArrayMesh meshSegment = WGS84EllipsoidMeshGenerator.CreateEllipsoidMeshSegment(
-            (float)tile.Latitude,
-            (float)tile.Longitude,
-            (float)tile.LatitudeRange,
-            (float)tile.LongitudeRange
-        );
-
-        node.Chunk.MeshInstance = new MeshInstance3D { Mesh = meshSegment };
-        node.Chunk.Name = $"TerrainChunk_z{tile.ZoomLevel}_x{tile.LongitudeTileCoo}_y{tile.LatitudeTileCoo}";
-        node.Chunk.Load();
-        AddChild(node.Chunk);
-    }
-
-
-    public TerrainQuadTreeNode GetCenter(int zoomLevel)
-    {
-        if (zoomLevel >= m_nullIslandNodes.Count)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        return m_nullIslandNodes[zoomLevel];
     }
 }
--- Universe/SolarSystem/Planets/Earth/Earth.cs
+++ Universe/SolarSystem/Planets/Earth/Earth.cs
@@ -27,106 +27,60 @@ public partial class Earth : Planet
 
     [Export] private Vector3 m_nullIsland;
 
-    float[] lodSplitThresholds =
-    {
-        80000f, // Zoom Level 0
-        40000f, // Zoom Level 1
-        20000f, // Zoom Level 2
-        10000f, // Zoom Level 3
-        5000f, // Zoom Level 4
-        2500f, // Zoom Level 5
-        1250f, // Zoom Level 6
-        600f, // Zoom Level 7
-        300f, // Zoom Level 8
-        150f, // Zoom Level 9
-        75f, // Zoom Level 10
-        38f, // Zoom Level 11
-        19f, // Zoom Level 12
-        9.5f, // Zoom Level 13
-        4.8f, // Zoom Level 14
-        2.4f, // Zoom Level 15
-        1.2f, // Zoom Level 16
-        0.6f, // Zoom Level 17
-        0.3f, // Zoom Level 18
-        0.15f // Zoom Level 19
-    };
-
-    private int m_currentZoomLevel = 6;
-
 
     public override void _Ready()
     {
-        m_planetID = PlanetID.EARTH;
-        m_planetShapeType = PlanetShapeType.WGS84_ELLIPSOID;
-        m_defaultZoomLevel = 6;
-        base._Ready();
-
-        InitializeCamera();
+        // m_planetID = PlanetID.EARTH;
+        // m_planetShapeType = PlanetShapeType.WGS84_ELLIPSOID;
+        // m_defaultZoomLevel = 6;
+        // base._Ready();
+        //
+        // InitializeCamera();
     }
 
     protected override void InitializePlanetDimensions()
     {
-        m_semiMajorAxisKm = SolarSystemConstants.EARTH_SEMI_MAJOR_AXIS_LEN_KM;
-        m_semiMinorAxisKm = SolarSystemConstants.EARTH_SEMI_MINOR_AXIS_LEN_KM;
+        // m_semiMajorAxisKm = SolarSystemConstants.EARTH_SEMI_MAJOR_AXIS_LEN_KM;
+        // m_semiMinorAxisKm = SolarSystemConstants.EARTH_SEMI_MINOR_AXIS_LEN_KM;
     }
 
     protected override void InitializePlanetSurface(int zoomLevel)
     {
-        m_terrainQuadTree = new TerrainQuadTree();
-        m_terrainQuadTree.InitializeQuadTree(zoomLevel);
-        m_currentZoomLevel = zoomLevel;
+        // m_terrainQuadTree = new TerrainQuadTree();
+        // m_terrainQuadTree.InitializeQuadTree(zoomLevel);
     }
 
     private void InitializeCamera()
     {
-        m_planetOrbitalCamera = GetNode<PlanetOrbitalCamera>("EarthOrbitalCamera");
-        m_planetOrbitalCamera.OrbitalCameraPosChanged += OnOrbitalCameraPosChangedSignal;
-
-        var nullIslandNode = m_terrainQuadTree.GetCenter(m_defaultZoomLevel - 1);
-
-        var surfaceArrays = nullIslandNode.Chunk.MeshInstance.Mesh.SurfaceGetArrays((int)Mesh.ArrayType.Vertex);
-
-        Vector3[] vertices = surfaceArrays[0].AsVector3Array();
-
-        m_nullIsland =
-            vertices.Aggregate(Vector3.Zero, (currTotalVec, currVec) => currTotalVec + currVec) / vertices.Length;
-
-        // TODO(Argyraspides, 08/02/2025): This is somewhat of a deep issue. I suspect that the mesh generator when using the trig functions
-        // is going counterclockwise/clockwise, while whatever direction is used to measure longitude is going in the oppostie direction. Meaning
-        // that our null island point (lat/lon of 0,0) is actually on the opposite side (0, 2pi), so we have to flip it here to actually face
-        // Africa off the coast of Guinea. Make sure to fix this. This may have implications as well with the map shader as
-        // that currently needs to be manually flipped inside of TerrainChunk rather than being taken care of in the shader
-        // directly
-        // Camera will be directly on null island. Push back 15,000km
-        m_planetOrbitalCamera.InitializeCameraPosition(-m_nullIsland + new Vector3(-15000, 0, 0));
+        // m_planetOrbitalCamera = GetNode<PlanetOrbitalCamera>("EarthOrbitalCamera");
+        // m_planetOrbitalCamera.OrbitalCameraPosChanged += OnOrbitalCameraPosChangedSignal;
+        //
+        // var nullIslandNode = m_terrainQuadTree.GetCenter(m_defaultZoomLevel - 1);
+        //
+        // var surfaceArrays = nullIslandNode.Chunk.MeshInstance.Mesh.SurfaceGetArrays((int)Mesh.ArrayType.Vertex);
+        //
+        // Vector3[] vertices = surfaceArrays[0].AsVector3Array();
+        //
+        // m_nullIsland =
+        //     vertices.Aggregate(Vector3.Zero, (currTotalVec, currVec) => currTotalVec + currVec) / vertices.Length;
+        //
+        // // TODO(Argyraspides, 08/02/2025): This is somewhat of a deep issue. I suspect that the mesh generator when using the trig functions
+        // // is going counterclockwise/clockwise, while whatever direction is used to measure longitude is going in the oppostie direction. Meaning
+        // // that our null island point (lat/lon of 0,0) is actually on the opposite side (0, 2pi), so we have to flip it here to actually face
+        // // Africa off the coast of Guinea. Make sure to fix this. This may have implications as well with the map shader as
+        // // that currently needs to be manually flipped inside of TerrainChunk rather than being taken care of in the shader
+        // // directly
+        // // Camera will be directly on null island. Push back 15,000km
+        // m_planetOrbitalCamera.InitializeCameraPosition(-m_nullIsland + new Vector3(-15000, 0, 0));
     }
 
     public override void LoadPlanet()
     {
-        AddChild(m_terrainQuadTree);
+        // AddChild(m_terrainQuadTree);
     }
 
     // TODO: Bruh idek anymore just do this tomorrow im too tired
     public void OnOrbitalCameraPosChangedSignal(Vector3 position)
     {
-        float minLatRange = m_planetOrbitalCamera.CurrentLat - m_planetOrbitalCamera.ApproxVisibleLatRadius;
-        float maxLatRange = m_planetOrbitalCamera.CurrentLat + m_planetOrbitalCamera.ApproxVisibleLatRadius;
-
-        float minLonRange = m_planetOrbitalCamera.CurrentLon - m_planetOrbitalCamera.ApproxVisibleLonRadius;
-        float maxLonRange = m_planetOrbitalCamera.CurrentLon + m_planetOrbitalCamera.ApproxVisibleLonRadius;
-
-        int minLatTile = MapUtils.LatitudeToTileCoordinateMercator(minLatRange, m_currentZoomLevel);
-        int maxLatTile = MapUtils.LatitudeToTileCoordinateMercator(maxLatRange, m_currentZoomLevel);
-
-        int minLonTile = MapUtils.LongitudeToTileCoordinateMercator(minLonRange, m_currentZoomLevel);
-        int maxLonTIle = MapUtils.LongitudeToTileCoordinateMercator(maxLonRange, m_currentZoomLevel);
-
-        for (int i = minLatTile; i <= maxLatTile; i++)
-        {
-            for (int j = minLonTile; j <= maxLonTIle; j++)
-            {
-                m_terrainQuadTree.SplitAndLoadQuadTreeNode(5, 5, 5);
-            }
-        }
     }
 }
