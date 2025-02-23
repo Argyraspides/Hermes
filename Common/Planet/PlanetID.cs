47eaf9b0617ffd3a0d3ff6b4d0983e345f321770
Argyraspides <joshivinayak441@gmail.com>
2025-02-23 21:55:03 +1300

    Added flair

--- Common/Planet/PlanetID.cs
+++ Common/Planet/PlanetID.cs
@@ -3,19 +3,18 @@
 
 
 
-  ,ad8888ba,         db         88         db
- d8"'    `"8b       d88b        88        d88b
-d8'                d8'`8b       88       d8'`8b
-88                d8'  `8b      88      d8'  `8b
-88      88888    d8YaaaaY8b     88     d8YaaaaY8b
-Y8,        88   d8""""""""8b    88    d8""""""""8b
- Y8a.    .a88  d8'        `8b   88   d8'        `8b
-  `"Y88888P"  d8'          `8b  88  d8'          `8b
+     ,ad8888ba,         db         88         db
+    d8"'    `"8b       d88b        88        d88b
+   d8'                d8'`8b       88       d8'`8b
+   88                d8'  `8b      88      d8'  `8b
+   88      88888    d8YaaaaY8b     88     d8YaaaaY8b
+   Y8,        88   d8""""""""8b    88    d8""""""""8b
+    Y8a.    .a88  d8'        `8b   88   d8'        `8b
+     `"Y88888P"  d8'          `8b  88  d8'          `8b
 
-                WEAVER OF THE WORLD
+                     WEAVER OF WORLDS
 
 */
-
 namespace Hermes.Common.Planet;
 
 public enum PlanetID
--- Universe/UI/UIComponents/CameraDistanceLabel.cs
+++ Universe/UI/UIComponents/CameraDistanceLabel.cs
@@ -1,3 +1,19 @@
+/*
+
+          db         88888888ba     ,ad8888ba,    88           88           ,ad8888ba,
+         d88b        88      "8b   d8"'    `"8b   88           88          d8"'    `"8b
+        d8'`8b       88      ,8P  d8'        `8b  88           88         d8'        `8b
+       d8'  `8b      88aaaaaa8P'  88          88  88           88         88          88
+      d8YaaaaY8b     88""""""'    88          88  88           88         88          88
+     d8""""""""8b    88           Y8,        ,8P  88           88         Y8,        ,8P
+    d8'        `8b   88            Y8a.    .a8P   88           88          Y8a.    .a8P
+   d8'          `8b  88             `"Y8888Y"'    88888888888  88888888888  `"Y8888Y"'
+
+
+
+
+*/
+
 namespace Hermes.Universe.UI.UIComponents;
 
 using Godot;
