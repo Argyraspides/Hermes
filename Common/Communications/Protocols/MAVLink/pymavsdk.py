e897501cf3143dafaa46206401153d20efa0c517
Argyraspides <joshivinayak441@gmail.com>
2025-02-26 00:05:44 +1300

    Added more instructions in pymavsdk.py

--- Common/Communications/Protocols/MAVLink/pymavsdk.py
+++ Common/Communications/Protocols/MAVLink/pymavsdk.py
@@ -36,12 +36,22 @@ from typing import Deque, Dict, List, Set, Any, Optional
 # - Function to take a MAVLink dictionary message and convert it to a JSON format
 # - Function to take a MAVLink JSON message, and serialize it back into a bytestream (in a way that is suitable to be sent
 # over UDP/TCP/Serial)
+# - Function to take a Hermes state message, and then serialize it into a bytestream (in a way that is suitable to be sent
+# # over UDP/TCP/Serial)
 
 # Python class, "MAVLinkWebSocket" that will
 # - Initialize a WebSocket immediately. The URL should be a global variable
 # - Take the next MAVLink dictionary message from the queue
-# - Convert this to a JSON string representation
+# - Convert this to a JSON string representation using the MAVLinkSerializer class functions
 # - Send the JSON string over the WebSocket
+# - Listen for Hermes' requesting to send MAVLink messages to the drones. Any message Hermes sends can be assumed to be a
+# message that it wants to send out to a drone
+# - Right now it is unclear what format this message will be in. Remember that Hermes is protocol-agnostic. Thus in the interest
+# of the cleanest architecture, Hermes will simply send out its "proprietary" internal format as-is. The Python script will have to
+# deal with this, serialize it, and send the message out via UDP/TCP/Serial. It might be best to just have Hermes have its own
+# module where it can convert a State object to a JSON string, and just hand it over to the Python script like that. This makes
+# sense since Hermes knows its own format best, so its better to convert to a JSON over that and then send it here for conversion back
+# to MAVLink and deserialization
 
 # Other things to keep in mind:
 # The user will want to configure UDP/TCP/Serial ports they wanna listen in on. The only real way to do this is to either have this Python script
