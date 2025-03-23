e0141b1d4ae52d36fbeb0bbeec5361aca7cddd92
Argyraspides <joshivinayak441@gmail.com>
2025-03-23 17:46:46 +1300

    Ensure compass needle pivot set properly on startup

--- Languages/ConcreteConversions/ToHellenic/MAVLinkToHellenicTranslator.cs
+++ Languages/ConcreteConversions/ToHellenic/MAVLinkToHellenicTranslator.cs
@@ -24,8 +24,7 @@ class MAVLinkToHellenicTranslator
         }
 
         // No suitable translation function found
-        Console.WriteLine("Unable to translate MAVLink message! No suitable translation function found for msgid: " +
-                          msgId);
+        // Console.WriteLine("Unable to translate MAVLink message! No suitable translation function found for msgid: " + msgId);
         return new List<HellenicMessage>();
     }
 
--- Universe/UI/UIComponents/CompassDisplay/CompassDisplay.cs
+++ Universe/UI/UIComponents/CompassDisplay/CompassDisplay.cs
@@ -19,12 +19,15 @@ public partial class CompassDisplay : Control
         m_compassLabelContainer = GetChild<CenterContainer>(1);
         EastWest = GetChild<HBoxContainer>(2);
         NorthSouth = GetChild<VBoxContainer>(3);
+
+        // Make sure the compass needle rotates around the triangle center
+        m_compassNeedleContainer.PivotOffset = m_compassNeedleContainer.Size / 2;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
         m_compassLabelContainer.GetChild<RichTextLabel>(0).Text = $"[center] {(int)HeadingDeg}\u00b0[/center]";
-        m_compassNeedleContainer.RotationDegrees = (float)HeadingDeg;
+            m_compassNeedleContainer.RotationDegrees = (float)HeadingDeg;
     }
 }
