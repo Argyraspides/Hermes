3a62068de93383226f0b9c58cc3a4e3884ee52e5
Argyraspides <joshivinayak441@gmail.com>
2025-05-04 15:29:14 +1200

    Added slide to confirm slider

--- Universe/UI/UIComponents/UIConstants.cs
+++ Universe/UI/UIComponents/UIConstants.cs
@@ -4,5 +4,20 @@ namespace Hermes.Universe.UI.UIComponents;
 
 public static class UIConstants
 {
+    // ***************************************************************
+    //
+    // UI size constants
+    //
+    // ***************************************************************
     public const float CONTROL_PANEL_MAX_HEIGHT = 200;
+
+    // ***************************************************************
+    //
+    // UI color constants
+    //
+    // ***************************************************************
+    public static readonly Color SepiaBlack = new Color("#300404");
+    public static readonly Color RedBerry = new Color("#8D0404");
+    public static readonly Color GuardsmanRed = new Color("#C30404");
+    public static readonly Color RedOxide = new Color("#670404");
 }
