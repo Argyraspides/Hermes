c537ac2bb883953c126417707617e446c2564232
Argyraspides <joshivinayak441@gmail.com>
2025-05-04 16:01:28 +1200

    Removed redundant setting

--- Universe/UI/UIComponents/MachineControl/LandControlComponent/LandControlComponent.cs
+++ Universe/UI/UIComponents/MachineControl/LandControlComponent/LandControlComponent.cs
@@ -28,7 +28,6 @@ public partial class LandControlComponent : HBoxContainer
         m_landButton = GetNode<TextureButton> ("LandButtonContainer/LandButton");
 
         m_landButtonContainer = GetNode<VBoxContainer>("LandButtonContainer");
-        // m_landButtonContainer.CustomMinimumSize = new Vector2(100, UIConstants.CONTROL_PANEL_MAX_HEIGHT);
 
         m_commander = new HellenicCommander();
     }
