12865479315a2d0fbebbc0d40d8c129716d9e264
Argyraspides <joshivinayak441@gmail.com>
2025-10-10 19:20:25 +1300

    Removed uneeded scope

--- External/setup_external.py
+++ External/setup_external.py
@@ -1,7 +1,7 @@
 import os, re
 
-old_path="res://"
-new_path = ""
+old_path: str = "res://"
+new_path: str = ""
 
 ignore_file_extensions = [".png", ".jpeg", ".jpg", ".ttf", ".otf", ".py"]
 
@@ -10,21 +10,19 @@ for (root, dirs, file_names) in os.walk(os.getcwd()):
         if any(extension in file_name for extension in ignore_file_extensions):
             continue
 
-        full_file_path = os.path.join(root, file_name)
+        full_file_path: str = os.path.join(root, file_name)
 
-        package_name_start_idx = full_file_path.rfind("External/")
-        cut_path = full_file_path[package_name_start_idx + len("External/"):]
-        next_slash_pos = cut_path.find("/")
+        package_name_start_idx: int = full_file_path.rfind("External/")
 
+        cut_path: str = full_file_path[package_name_start_idx + len("External/"):]
+
+        next_slash_pos: int = cut_path.find("/")
         if next_slash_pos != -1:
             curr_package_name = cut_path[:next_slash_pos]
             new_path = old_path + "External/" + curr_package_name + "/"
 
-        new_file_content: str = ""
         with open(full_file_path, 'r') as file:
             old_file_content = file.read()
-            if old_path not in old_file_content:
-
             new_file_content = old_file_content.replace(old_path, new_path)
 
         with open(full_file_path, 'w') as file:
